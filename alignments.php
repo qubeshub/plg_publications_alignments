a00f7d2679ab76648f7a8260ed8cbdefa01c017c
M. Drew LaMar <mdlama@wm.edu>
2023-12-20 16:42:31 -0500

    [feat] Cloud focus areas now ordered by alignment
    (cherry picked from commit bfc210575c6b2bf2849b1fd87bbd5dd436989bc6)

--- alignments.php
+++ alignments.php
@@ -97,9 +97,11 @@ class plgPublicationsAlignments extends \Qubeshub\Plugin\Plugin
 		}
 
 		// Metadata
+		$mtype = $publication->master_type;
 		$tags = $cloud->tags('list', array("type" => "focusareas"), true);
 		$fas = \Components\Tags\Models\FocusArea::fromTags($tags);
-		$roots = $fas->parents(true); // Get root FAs
+		echo var_dump($fas->parents(true)->getTableColumns()); die;
+		$roots = $fas->parents(true)->orderByAlignment($mtype, 'P');
 		$view = $this->view('default', 'metadata')
 			->set('publication', $publication)
 			->set('fas', $roots);
